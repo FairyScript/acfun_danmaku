93029606ddf2135b25dafdf2046a4a6083e236fd
FairyScript <h@queb.fun>
2020-07-18 09:43:06 +0800

    Add files via upload

---
+++ acfun_danmaku.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         ACFun弹幕姬(仮)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://live.acfun.cn/live/*
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  //const css = CSSF();
+  //$('body').append($('<style>').html(css));
+
+  let msgCount = 0;
+  setTimeout(function () {
+    console.log('start');
+    const messageContainer = $('.live-feed-messages');
+    messageContainer.on('scroll',function(){
+      const msg = $(this).children();
+      if(msg.length !== msgCount){
+        msgCount = msg.length;
+        const lastMsg = msg.last();
+        const msgType = lastMsg.attr('class');
+        switch(msgType){
+          case 'gift': {
+            const giftText = lastMsg.find('span').last().html();
+            if(/香蕉/.test(giftText)) lastMsg.children('div').attr('class','gift-banana');
+          }
+        }
+      }
+    });
+},2000);
+})();
